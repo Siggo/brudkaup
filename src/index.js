c647fea7275289a58768baa12f31966d54114d00
siggo <sigtrygguroh@gmail.com>
2018-01-26 00:50:27 +0000

    Grunnútlit

--- src/index.js
+++ src/index.js
@@ -3,32 +3,47 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import {  BrowserRouter } from 'react-router-dom'
-
+import { Route, NavLink, HashRouter } from 'react-router-dom'
 
 import configureStore from './redux/store'
 
 import { Provider } from 'react-redux'
 
-import MainLayout from './layout/MainLayout'
-
 import Main from './pages/main'
+import About from './pages/about'
+import Stuff from './pages/stuff'
+
+import banner from './images/Banner.jpg'
 
 import actions from './redux/actions/'
 
-import './styles/main.css'
+import './styles/dist/main.css'
 
 const store = configureStore()
 
-function routeUpdate () {
-  window.scrollTo(0, 0)
-}
-
 render(
   <Provider store={store}>
-      <BrowserRouter onUpdate={routeUpdate.bind(this)}>
-     	<MainLayout path="/" component={Main} />
-      </BrowserRouter>
+    <HashRouter>
+      <div>
+        <div className='header'>
+          <ul className='header--nav'>
+            <li><NavLink exact to='/'>Main</NavLink></li>
+            <li><NavLink to='/stuff'>Stuff</NavLink></li>
+            <li><NavLink to='/about'>About</NavLink></li>
+          </ul>
+        </div>
+        <div className='banner'>
+          <img src={banner} alt='banner' />
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <Route exact path='/' component={Main} />
+            <Route path='/stuff' component={Stuff} />
+            <Route path='/about' component={About} />
+          </div>
+        </div>
+      </div>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 )
