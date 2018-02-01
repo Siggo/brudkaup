50bf83978ba0a11b42cf360299a73db889de54ee
Sigtryggur Óskar Hrafnkelsson <sigtryggur.oskar.hrafnkelsson@advania.is>
2018-02-01 11:37:07 +0000

    Bug fix

--- src/layouts/container.js
+++ src/layouts/container.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
 const Container = ({component: Component, id}) => {
   return (
     <div className='subpage' id={id}>
