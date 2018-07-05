85fccd883467707ada90cd154ed36020ec164a52
siggo <sigtrygguroh@gmail.com>
2018-07-05 13:59:36 +0000

    rsvp stöff

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -18,7 +18,7 @@ class RSVP extends Component {
      $.ajax({
        url: "https://docs.google.com/forms/d/e/1FAIpQLSdRmYai8ze-PcJyFQ6mYFiM15jIcnABxJX-O-Gs6LT5XzoJAw/formResponse",
        data: {
-         "entry.877086558": $("#rsvp-form input[type='radio'][name='rsvp']:checked").val(),
+         "entry.877086558": $(".rsvp-form input[type='radio'][name='rsvp']:checked").val(),
          "entry.1498135098": $('#names').val(),
          "entry.2606285": $('#comment').val()
        },
@@ -42,23 +42,26 @@ class RSVP extends Component {
       <div>
         <h2>RSVP</h2>
         { !this.state.registered && 
-          <form id="rsvp-form" target="_self" onSubmit={this.postToGoogle.bind(this)}>
-            <fieldset>
-              <p>Sérðu þér fært að komast?</p>
+          <form className="rsvp-form" target="_self" onSubmit={this.postToGoogle.bind(this)}>
+            
+              <p>Það mikilvægasta! Ef þú ert hérna inni getum við ekki ímyndað okkur gott brúðkaup án þín. Smelltu inn svari að neðan hvort þú komist og nöfnin á þeim sem að mæta.</p>
+              <p>Við minnum góðlátlega á að það verður veisla fram á kvöld þannig að yngra fólkið (undir 18) þarf að vera heima.</p>
+              <fieldset>
               <input id="qs1_op_1" type="radio" value="Já, ég mæti!" name="rsvp" />
               <label for="qs1_op_1">Já, ég mæti!</label>
               <input id="qs1_op_2" type="radio" value="Því miður kemst ég ekki" name="rsvp" />
               <label for="qs1_op_1">Kemst því miður ekki</label>
             </fieldset>
-
-            <fieldset>
-              <p>Hver eru nöfn þeirra sem mæta?</p>
-              <textarea id="names" name="names"></textarea>
-            </fieldset>
-            <fieldset>
-              <p>Athugasemdir eða spurningar</p>
-              <textarea id="comment" name="comment"></textarea>
-            </fieldset>
+            <div className='rsvp-form__text'>
+              <fieldset>
+                <p>Hver eru nöfn þeirra sem mæta?</p>
+                <textarea id="names" name="names"></textarea>
+              </fieldset>
+              <fieldset>
+                <p>Athugasemdir eða spurningar</p>
+                <textarea id="comment" name="comment"></textarea>
+              </fieldset>
+            </div>
               <button id="send" disabled={this.state.sending} type="submit">
                 Senda
               </button>
